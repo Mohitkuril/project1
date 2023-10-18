770aad4a9f7874e64e962dc4cc223afc8f76b216
Mohitkuril <128623074+Mohitkuril@users.noreply.github.com>
2023-10-18 12:35:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+import Dashboard from "./Dashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
